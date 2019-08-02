9838d201523469ecfa95bc1e247c8ffcfa05cf9e
Ryo Nakamura <upa@haeena.net>
2019-08-02 21:32:39 +0900

    separate tap_read process from callback thread

--- device/nettlp_snic_device.c
+++ device/nettlp_snic_device.c
@@ -12,9 +12,14 @@
 #include <arpa/inet.h>
 #include <linux/if.h>
 #include <linux/if_tun.h>
+#include <signal.h>
+#include <poll.h>
+#include <pthread.h>
 
 #include <libtlp.h>
 #include <nettlp_snic.h>
+
+static int caught_signal = 0;
 
 struct nettlp_snic {
 
@@ -23,6 +28,17 @@ struct nettlp_snic {
 	/* filled by message API */
 	uintptr_t bar4_start;
 	struct nettlp_msix tx_irq, rx_irq;
+
+
+	/* XXX: should be protected among threads by mutex */
+	int rx_state;
+#define RX_STATE_INIT	0
+#define	RX_STATE_READY	1
+#define RX_STATE_BUSY	2
+#define RX_STATE_DONE	3
+	uintptr_t rx_desc_addr;
+	struct descriptor rx_desc;
+	struct nettlp *rx_nt;
 };
 
 #define BAR4_TX_DESC_OFFSET	0
@@ -38,7 +54,7 @@ struct nettlp_snic {
 int nettlp_snic_mwr(struct nettlp *nt, struct tlp_mr_hdr *mh,
 		    void *m, size_t count, void *arg)
 {
-	int ret, pktlen;
+	int ret;
 	struct nettlp_snic *snic = arg;
 	struct descriptor desc;
 	uintptr_t dma_addr, addr;
@@ -93,44 +109,88 @@ int nettlp_snic_mwr(struct nettlp *nt, struct tlp_mr_hdr *mh,
 
 	} else if (is_mwr_addr_rx_desc_ptr(snic->bar4_start, dma_addr)) {
 
+		/* wait untile last DMA write done */
+		while (snic->rx_state != RX_STATE_DONE &&
+		       snic->rx_state != RX_STATE_INIT)
+			sched_yield();	
+
 		/* 1. RX descriptor is udpated. start RX process */
-		memcpy(&addr, m, sizeof(addr));
-		printf("dma to %#lx, rx desc ptr is %#lx\n", dma_addr, addr);
+		memcpy(&snic->rx_desc_addr, m, sizeof(snic->rx_desc_addr));
+		printf("dma to %#lx, rx desc ptr is %#lx\n",
+		       dma_addr, snic->rx_desc_addr);
 
 		/* 2. Read descriptor from host */
-		ret = dma_read(nt, addr, &desc, sizeof(desc));
-		if (ret < sizeof(desc)) {
+		ret = dma_read(nt, snic->rx_desc_addr, &snic->rx_desc,
+			       sizeof(snic->rx_desc));
+		if (ret < sizeof(snic->rx_desc)) {
 			fprintf(stderr, "failed to read rx desc from %#lx\n",
 				addr);
 			return -1;
 		}
 
-		/* 2.5. read a packet from the tap interface */
+		/* 2.1. Set RX State Ready because we have new buffer  */
+		snic->rx_nt = nt;
+		snic->rx_state = RX_STATE_READY;
+	}
+
+	return 0;
+}
+
+
+void *nettlp_snic_tap_read_thread(void * arg)
+{
+	int ret, pktlen;
+	char buf[2048];
+	struct nettlp_snic *snic = arg;
+	struct pollfd x[1] = {{ .fd = snic->fd, .events = POLLIN}};
+
+	/* This is the actual part of RX. This thread read tap socket,
+	 * and if rx buffer is available, DMA Write the packet from
+	 * the tap to the RX buffer on the NetTLP adapter host.
+	 */
+
+	while (1) {
+
+		if (caught_signal)
+			break;
+
+		ret = poll(x, 1, 500);
+		if (ret < 0 || ret == 0 || !(x[0].revents & POLLIN))
+			continue;
+
+		/* 2.2. read a packet from the tap interface */
 		pktlen = read(snic->fd, buf, sizeof(buf));
 		if (pktlen < 0) {
 			perror("read");
-			return -1;
+			continue;
 		}
 
+		if (snic->rx_state != RX_STATE_READY)
+			continue;
+
+		snic->rx_state = RX_STATE_BUSY;
+
 		/* 3. DMA the packet to host */
-		ret = dma_write(nt, desc.addr, buf, pktlen);
+		ret = dma_write(snic->rx_nt, snic->rx_desc.addr, buf, pktlen);
 		if (ret < 0) {
 			fprintf(stderr, "failed to write rx pkt to %#lx\n",
-				desc.addr);
-			return -1;
+				snic->rx_desc.addr);
+			continue;
 		}
 
 		/* 4. Write back RX descriptor */
-		desc.length = pktlen;
-		ret = dma_write(nt, addr, &desc, sizeof(desc));
-		if (ret < sizeof(desc)) {
+		snic->rx_desc.length = pktlen;
+		ret = dma_write(snic->rx_nt, snic->rx_desc_addr,
+				&snic->rx_desc, sizeof(snic->rx_desc));
+		if (ret < sizeof(snic->rx_desc)) {
 			fprintf(stderr, "failed to write rx desc to %#lx\n",
-				addr);
-			return -1;
+				snic->rx_desc_addr);
+			continue;
 		}
 
 		/* 5. Generate RX interrupt */
-		ret = dma_write(nt, snic->rx_irq.addr, &snic->rx_irq.data,
+		ret = dma_write(snic->rx_nt, snic->rx_irq.addr,
+				&snic->rx_irq.data,
 				sizeof(snic->rx_irq.data));
 		if (ret < 0) {
 			fprintf(stderr, "failed to generate RX interrupt\n");
@@ -138,11 +198,15 @@ int nettlp_snic_mwr(struct nettlp *nt, struct tlp_mr_hdr *mh,
 		}
 
 		printf("rx done. DMA write to %#lx %d byte\n\n",
-		       desc.addr, pktlen);
+		       snic->rx_desc.addr, pktlen);
+
+		snic->rx_state = RX_STATE_DONE;
 	}
 
-	return 0;
+	return NULL;
 }
+
+
 
 int tap_alloc(char *dev)
 {
@@ -193,6 +257,11 @@ int tap_up(char *dev)
 }
 
 
+void sig_handler(int sig)
+{
+	caught_signal = 1;
+	nettlp_stop_cb();
+}
 
 void usage(void)
 {
@@ -216,6 +285,7 @@ int main(int argc, char **argv)
 	struct nettlp_snic snic;
 	struct in_addr host;
 	struct nettlp_msix msix[2];	/* tx and rx interrupt */
+	pthread_t rx_tid;	/* tap_read_thread */
 
 	memset(&nt, 0, sizeof(nt));
 	busn = 0;
@@ -308,10 +378,25 @@ int main(int argc, char **argv)
 	printf("RX IRQ address is %#lx, data is 0x%08x\n", snic.rx_irq.addr,
 	       snic.rx_irq.data);
 
+        /* set signal handler to stop callback threads */
+        if (signal(SIGINT, sig_handler) == SIG_ERR) {
+		perror("cannot set signal\n");
+		return -1;
+        }
+
+	/* start RX thread */
+	printf("create tap read thread\n");
+	pthread_create(&rx_tid, NULL, nettlp_snic_tap_read_thread, &snic);
+
 	/* start nettlp call back */
+	printf("start nettlp callback\n");
 	memset(&cb, 0, sizeof(cb));
 	cb.mwr = nettlp_snic_mwr;
 	nettlp_run_cb(nts_ptr, 16, &cb, &snic);
+
+	printf("nettlp callback done\n");
+
+	pthread_join(rx_tid, NULL);
 
 	return 0;
 }
